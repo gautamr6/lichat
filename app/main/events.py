486cb85d3a141473414c2b1caf3d2865f68c8a17
Stuart Rucker <stuartrucker@Stuarts-MacBook-Air.local>
2023-01-21 12:35:57 -0800

    d

--- app/main/events.py
+++ app/main/events.py
@@ -1,15 +1,22 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
-from .. import socketio
 
+from .. import socketio
+import chats
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
     room = session.get('room')
+    situation_dict = chats.get_situation()
+    robot_response = chats.get_dialog(situation_dict)
+    session['situation'] = situation_dict
     join_room(room)
-    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    emit('status', {
+        'msg': 'Situation: ' + situation_dict['situation_description'] + '\n' + robot_response }, 
+        room=room
+    )
 
 
 @socketio.on('text', namespace='/chat')
---
+++ chats.py
@@ -0,0 +1,23 @@
+import openai
+
+openai.api_key = "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"
+
+def get_situation():
+
+    return {
+        'situation_description': 'An old woman has just lost a cat and has asked for your help to find it. You are very busy and need to be on your way. You must deny her',
+        'robot_name': 'Old Woman'
+    }
+
+def get_dialog(situation_dict):
+    prompt = f"{situation_dict['situation_description']}\n{situation_dict['robot_name']}:"
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    message = completions.choices[0].text
+    return message
