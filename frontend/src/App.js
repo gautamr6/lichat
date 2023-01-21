10b2c3809e0c8302b55bb61b8aa4e868ccd62d08
Gautam Ramesh <gautamshivramesh@gmail.com>
2023-01-21 14:31:16 -0800

    frontend socket attempt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import openSocket from 'socket.io-client';
 
 function App() {
   const [users, setUsers] = useState(0);
   const [chatHistory, setChatHistory] = useState([{me: false, text: "awfawaf"}, {me: true, text: "awefawe"}]);
   const [newMessage, setNewMessage] = useState('');
 
+  const temp = openSocket('http://localhost:8000/chat');
+  const socket = temp.connect('http://localhost:8000/chat');
+  useEffect(() => {
+    socket.on('text', data => {
+        console.log("hi")
+        setChatHistory([...chatHistory, data.msg]);
+    });
+    return () => {
+        socket.disconnect();
+    }
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    setChatHistory([...chatHistory, newMessage]);
+    console.log("emit");
+    socket.emit('text', newMessage);
     setNewMessage('');
   }
 
   return (
-    <div class="container">
+    <div className="container">
       <div style={{marginLeft: 'auto'}}>Users: {users}</div>
-      <div class="mid">
-        <div class="prompt">
+      <div className="mid">
+        <div className="prompt">
           <span>Prompt</span>
         </div>
-        <div class="chat">
+        <div className="chat">
           {chatHistory.map((message, index) => (
             <div key={index} style={message.me ? {marginLeft: 'auto'} : {}}>{message.text}</div>
           ))}
